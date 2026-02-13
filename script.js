2acc17eb6c344a5f95bfb2c6bdc19ba7b76a0f44
POOJA12345915 <giriyannanavarpooja@gmail.com>
2026-02-14 00:04:41 +0530

    update UI

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function add(item){
-  let cart = document.getElementById("cart");
-  let li = document.createElement("li");
-  li.innerText = item;
-  cart.appendChild(li);
+ let cart=document.getElementById("cart");
+ let li=document.createElement("li");
+ li.innerHTML="✅ "+item;
+ cart.appendChild(li);
 }
